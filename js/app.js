c856599d84333831a901a06983bb78185b4895fc
claudio Monzoni <claudiomonzoni@hotmail.com>
2021-01-06 08:21:14 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,102 @@
+
+//only phones
+var x = window.matchMedia("(max-width: 479px)");
+esCell(x); // Call listener function at run time
+x.addListener(esCell); // Attach listener function on state changes
+
+// nav
+
+function esCell(x) {
+  if (x.matches) {
+    // If media query matches
+    const menuIco = document.querySelector(".menu-toggle");
+    const mainNav = document.querySelector("#mainNav");
+    const anchor = document.querySelector(".anchor");
+    const abierto = false;
+
+    let abrirMenuFull = () => {
+      this.abierto = !this.abierto;
+      mainNav.classList.remove("menuFullAbierto");
+      mainNav.classList.remove("desanimaMenu");
+      menuIco.classList.toggle("is-active");
+
+      this.abierto
+        ? mainNav.classList.toggle("menuFullAbierto")
+        : mainNav.classList.toggle("desanimaMenu");
+    };
+
+    menuIco.addEventListener("click", abrirMenuFull);
+    //anchor.addEventListener("click", abrirMenuFull);
+  } else {
+    return;
+  }
+}
+
+//submenu
+
+const productos = document.querySelector("#anchorpro")
+const submenu = document.querySelector(".submenu")
+const flecha = document.querySelector("#anchorpro span")
+const cerrar = document.querySelector(".cerrarCel")
+
+let escondido = true
+productos.addEventListener("click", function(e){
+e.preventDefault()
+submenu.classList.remove("submenuAbierto")
+submenu.classList.remove("submenuCerrado")
+escondido ? submenu.classList.toggle("submenuAbierto") : submenu.classList.toggle("submenuCerrado")
+if(escondido){
+  flecha.innerHTML="&#9660;"
+  submenu.classList.add("mostrar")
+}else{
+  flecha.innerHTML="&#9654;"
+}
+escondido = !escondido
+
+//animacion de css
+})
+
+cerrar.addEventListener("click", ()=>{
+submenu.classList.add("submenuCerrado")
+submenu.classList.remove("submenuAbierto")
+flecha.innerHTML="&#9654;"
+escondido=true
+})
+//carrusel
+
+
+//carrusel
+
+const cambio = () => {
+  console.log(`Actual ${this.currentSlide}`)
+}
+
+
+
+const carrusel = new Siema({
+  selector: ".slideHome",
+  easing: "ease-out",
+  //perPage: 2,
+  loop: true,
+  draggable: true,
+  perPage:{
+    768:2,
+    1024:3
+  },
+  onInit: cambio,
+  onChange: cambio,
+});
+
+const izq = document.querySelector("#izq")
+const der = document.querySelector("#der")
+
+izq.addEventListener("click", (e) => {
+  e.preventDefault()
+  carrusel.prev()
+})
+der.addEventListener("click", (e) => {
+  e.preventDefault()
+  carrusel.next()
+})
+
+
